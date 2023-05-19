4948bc9d2f322342f2857561e58adf71a200d63e
Oveln <oveln@outlook.com>
2023-05-19 19:30:15 +0800

    更改luogu4396

--- ICPC/20230516/luogu4396/main.cpp
+++ ICPC/20230516/luogu4396/main.cpp
@@ -1,30 +1,59 @@
 #include<cstdio>
 #include<cmath>
 #include<algorithm>
-#define maxn 100005
+#include<cstring>
+#define MAXN 100005
 
+//线段树结点
+struct TreeNode {
+  int sum,kinds; // 数量和种类数
+  TreeNode operator+(const TreeNode b) {
+    return TreeNode{sum+b.sum,kinds+b.kinds};
+  }
+};
 //用数组模拟线段树
 struct Tree{
-  int a[maxn];
-  void add(int L,int R,int x) {
-    for (int i=L;i<=R;i++) {
-      a[i]+=x;
+  TreeNode a[2*MAXN];
+  int size;
+
+  Tree(int x):size(x) {
+    memset(a,sizeof(a),0);
+  }
+  TreeNode _query(int now ,int L,int R, int QL, int QR) {
+    if (L==QL&&R==QR) {
+      return a[now];
     }
+    int mid = L+ ((R-L)>>1);
+    if (QR<=mid) return _query(now*2, L, mid, QL, QR);
+    else if (QL>mid) return _query(now*2+1, mid+1, R, QL, QR);
+    else return _query(now*2, L, mid, QL, mid)+_query(now*2+1, mid+1, R, mid+1, QR);
   }
-  int query(int x) {
-    return a[x]; 
+  //查询L到R的数据
+  TreeNode query(int L,int R) {
+    return _query(1,1,size,L,R);
   }
-  void debug() {
-    printf("debug: ");
-    for (int i=1;i<=5;i++) printf("%d ",a[i]);
-    printf("\n");
+  
+  void _set(int now,int L,int R,int pos,int value) {
+    if (L==R && L==pos) {
+      a[now].sum+=value,
+      a[now].kinds=a[now].sum>0;
+      return;
+    }
+    int mid = L+((R-L)>>1);
+    if (pos<=mid) _set(now*2, L, mid, pos, value);
+    else _set(now*2+1, mid+1, R, pos, value);
+    a[now] = a[now*2]+a[now*2+1];
   }
-}f;
+  void set(int pos,int value) {
+    _set(1,1,size,pos,value);
+  }
+
+}f(0);
 
 int n,m,block_size,maxai;
-int a[maxn],block[maxn];
+int a[MAXN],block[MAXN];
 struct question {
-  int l,r,a,b,id,ans;
+  int l,r,a,b,id,ans1,ans2;
   question() {}
   question(int l,int r,int a,int b,int id) {
     this->l=l;
@@ -38,17 +67,18 @@ struct question {
     if (block[l]==block[y.l]) return r<y.r;
     return block[l]<block[y.l];
   }
-}q[maxn];
+}q[MAXN];
 
 int nowL,nowR;
-void dec(int x) {
-  f.add(x,maxai,-1);
+void dec(int x, Tree* f) {
+  f->set(x, -1); 
 }
-void add(int x) {
-  f.add(x,maxai,1);
+void add(int x, Tree* f) {
+  f->set(x, 1);
 }
 int main() {
   scanf("%d%d",&n,&m);
+  f.size=n;
   block_size=sqrt(n);
   for (int i=1;i<=n;i++) {
     block[i]=i/block_size+1;
@@ -64,19 +94,20 @@ int main() {
   }
   std::sort(q+1,q+1+n);
   nowL=nowR=1;
-  add(a[1]);
+  add(a[1],&f);
   for (int i=1;i<=m;i++) {
-    while (nowL<q[i].l) dec(a[nowL++]);
-    while (nowL>q[i].l) add(a[--nowL]);
-    while (nowR>q[i].r) dec(a[nowR--]);
-    while (nowR<q[i].r) add(a[++nowR]);
-    q[i].ans=f.query(q[i].b)-f.query(q[i].a-1);
+    while (nowL<q[i].l) dec(a[nowL++],&f);
+    while (nowL>q[i].l) add(a[--nowL],&f);
+    while (nowR>q[i].r) dec(a[nowR--],&f);
+    while (nowR<q[i].r) add(a[++nowR],&f);
+    auto ans = f.query(q[i].a, q[i].b);
+    q[i].ans1 = ans.sum, q[i].ans2 = ans.kinds;
   }
   std::sort(q+1,q+1+n,
       [](question x,question y)->bool {return x.id<y.id;}
       );
   for (int i=1;i<=m;i++) {
-    printf("%d\n",q[i].ans);
+    printf("%d %d\n",q[i].ans1, q[i].ans2);
   }
   return 0;
 }
