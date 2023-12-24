f8075e5d46cf407ec98d780d89e5283ba69c9fc5
Oveln <oveln@outlook.com>
2023-12-24 22:23:21 +0800

    补E题的代码

---
+++ ICPC/20231222/E/E.cpp
@@ -0,0 +1,28 @@
+#include<cstdio>
+
+const int maxn=200005;
+int n,Q,q,maxai;
+int a[maxn];
+bool ans[maxn],vis[maxn];
+int max(int x,int y) {
+    if (x>y) return x;return y;
+}
+int main() {
+    scanf("%d%d",&n,&Q);
+    if (n==1) ans[1]=1;
+    for (int i=1;i<=n;i++) {
+        scanf("%d",&a[i]);
+        if (vis[a[i]]) ans[1]=1;
+        vis[a[i]]=1;
+        maxai=max(maxai,a[i]);
+    }
+    for (int i=1;i<=n;i++) {
+        for (int j=2;j*a[i]<=maxai;j++)
+            ans[j]|=vis[j*a[i]];
+    }
+    while (Q--) {
+        scanf("%d",&q);
+        printf("%s\n",ans[q]?"YES":"NO");
+    }
+    return 0;
+}
